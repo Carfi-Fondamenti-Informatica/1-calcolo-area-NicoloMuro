34794312ac413b1096e44dc7d15dd73124e2621a
NicoloMuro <100717733+NicoloMuro@users.noreply.github.com>
2022-03-16 02:30:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-//Qui vanno definite le variabili
+int main() {
+    float a=0,b=0,c=0,triangolo=0,quadrato=0,rettangolo=0,trapezio=0;
 
-cin >> a >> b >> c;
+    std::cout << "inserire tre valori reali" << std::endl;
+    cin >> a >> b >> c;
+    triangolo=a*b/2;
+    quadrato=a*a;
+    rettangolo=a*b;
+    trapezio=(a+b)*c/2;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cout <<a<< " base e " <<b<< " altezza del triangolo di area " <<triangolo<< endl;
+    cout <<a<< " lato del quadrato di area " <<quadrato<< endl;
+    cout <<a<< " lato minore e " <<b<< " lato maggiore del rettangolo di area " <<rettangolo<< endl;
+    cout <<a<< " base minore, " <<b<< " base maggiore e " <<c<< " altezza del trapezio di area " <<trapezio<< endl;
 
-
+    return 0;
 }
